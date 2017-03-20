35e52248eb0dc9114dc688e16646d8710ca8bb13
milselarch <milselarch@gmail.com>
2017-03-20 11:01:00 +0800

    first commit

---
+++ leaf_lights.ino
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(57600);
+    delay(3000);
+
+    analogWriteRange(255);
+    //pinMode(D0, OUTPUT);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    // fade in from min to max in increments of 5 points
+    
+    for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+        // sets the value (range from 0 to 255):
+        analogWrite(D0, fadeValue);
+        // wait for 30 milliseconds to see the dimming effect
+        delay(30);
+    }
+
+    Serial.println("test");
+    
+    // fade out from max to min in increments of 5 points:
+    for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+        // sets the value (range from 0 to 255):
+        analogWrite(D0, fadeValue);
+        // wait for 30 milliseconds to see the dimming effect
+        delay(30);
+    }
+}
