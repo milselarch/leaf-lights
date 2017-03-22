cec9b4f07646d2bcc4e6ebcd473c4a7fa4140140
milselarch <milselarch@gmail.com>
2017-03-22 22:45:10 +0800

    code is too complex now

--- leaf_lights.ino
+++ leaf_lights.ino
@@ -2,6 +2,17 @@
 #include "misc.h"
 #include "ultrasonic.h"
 
+/*
+float multiplier = 1;
+const float factor = 0.88;
+float cTotal = 1.0 / (1.0 - factor);
+*/
+
+fader fadeController;
+ultrasonic distSensor;
+
+unsigned long inTime = -1;
+
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(57600);
@@ -13,54 +24,17 @@ void setup() {
     pinMode(D6, INPUT); //ultrasonic echo recieve pin
 }
 
-fader fadeController;
-float multiplier = 1;
-const float factor = 0.88;
-float cTotal = 1.0 / (1.0 - factor);
-
-unsigned long inTime = -1;
-
 void loop() {
-    // put your main code here, to run repeatedly:
-    // fade in from min to max in increments of 5 points
-    //fadeInOut();
-    //randLED();
-
-    //fadeBlueGreen();
-
-    //Serial.println(getMedianValue());
-    //displayRGB(light);
-
     //float distance = readDistance();
-    float distance = getStableValue();
-    const int maxDistance = 45;
-    const int minDistance = 10;
+    float distance = distSensor.getStableValue();
 
-    if (maxDistance > distance and distance > minDistance) {
-        // people detected between max and min distance
-        const float base = 0.5;
-        /*
-        multiplier = base + (
-            (distance-minDistance)
-            /(maxDistance-minDistance)
-            )*(1.0-base)
-            ;
-        */
-
-        multiplier = 1;
-        inTime = millis();
-        
-    } else if (minDistance > distance) {
-        // inside min distance
-        multiplier = 1;
-
-        inTime = millis();
-        
-    } else if (distance > maxDistance) {
-        // distance outside max distance
-        // multiplier = 0.5;
-        multiplier = 1;
-    } 
+    if (distance != -1) {
+        const int maxDistance = 45;
+        if (maxDistance > distance) {
+            // people detected between max and min distance
+            inTime = millis();   
+        }
+    }
     
     if (millis() - inTime > 1000*20) {
         fadeController.off = true;
@@ -68,15 +42,8 @@ void loop() {
         fadeController.off = false;
     }
 
-    Serial.println(fadeController.off);
-    Serial.println(int(distance));
-    if (fadeController.off == false) {
-        for (int k = 0; k < 100; k++) {
-            fadeController.fadeInOut(30, multiplier);
-        }
-    } else {
-        fadeController.fadeInOut(30, multiplier);  
-    }
+    //Serial.println(fadeController.off);
+    fadeController.fadeInOut(0, 1); 
 }
 
 
--- ultrasonic.h
+++ ultrasonic.h
@@ -1,42 +1,74 @@
-float readDistance() {
-    digitalWrite(D5, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(D5, LOW);
-    int duration = pulseIn(D6, HIGH);
-    float distanceCm = duration*0.034/2;
-    float distanceInch = duration*0.0133/2;
-    delay(11);
-
-    return distanceCm;
-}
-
-float prevDistance = -100;
-
-float getStableValue() {    
-    float distance = readDistance();
-
-    while (true) {
-        if (abs(distance - prevDistance) < 2) {
-            return distance;
+class ultrasonic {
+public:
+    bool DEBUG = 1;
+    float prevDistance = -100;
+    
+    float readDistance() {
+        digitalWrite(D5, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(D5, LOW);
+        int duration = pulseIn(D6, HIGH);
+        float distanceCm = duration*0.034/2;
+        float distanceInch = duration*0.0133/2;
+        
+        if (duration < 10*1000) {
+            delayMicroseconds(10*1000 - duration);
+            return distanceCm;
+        } else {
+            return 165;
+        }
+        
+        //delay(11);
+    }
+    
+    float getStableValue() {    
+        float distance = readDistance();
+        float diff = abs(distance - prevDistance);
+        float abDiff = abs(
+            (pow(distance, 2) - pow(prevDistance, 2))
+            /(distance * prevDistance)
+            );
+        
+        if (diff < 2) {
+            if (DEBUG == true) {
+                Serial.print(diff);
+                Serial.print("d ");
+                Serial.print(prevDistance);
+            }
+            
+        } else if (abDiff < 0.5) {
+            if (DEBUG == true) {
+                Serial.print(abDiff);
+                Serial.print(": ");
+                Serial.print(prevDistance);
+            }
             
         } else {
             prevDistance = distance;
+            return -1;
         }
+
+        Serial.print(" ");
+        Serial.println(distance);
+        
+        prevDistance = distance;
+        return distance;
     }
-}
-
-void displayRGB(int RGB) {
-    int blue = 63 * (RGB % 4);
-    RGB /= 4;
-   
-    int green = 31 * (RGB % 8);
-    RGB /= 8;
-
-    int red = 31 * (RGB % 8);
-
-    analogWrite(D1, blue);
-    analogWrite(D2, red);
-    analogWrite(D3, green);
+    
+    void displayRGB(int RGB) {
+        int blue = 63 * (RGB % 4);
+        RGB /= 4;
+       
+        int green = 31 * (RGB % 8);
+        RGB /= 8;
+    
+        int red = 31 * (RGB % 8);
+    
+        analogWrite(D1, blue);
+        analogWrite(D2, red);
+        analogWrite(D3, green);
+    };
 };
+
 
 
